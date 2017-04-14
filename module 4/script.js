fcf17aa94cdd1028eb0dc8a399e2860a8c70b4e0
Jasmine.Cordero <jcordero91@gmail.com>
2017-04-14 19:02:59 -0400

    MEssage

--- module 4/script.js
+++ module 4/script.js
@@ -1,60 +1,19 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <title>Module 4 Solution Starter</title>
-  <script src="SpeakHello.js"></script>
-  <script src="SpeakGoodBye.js"></script>
-  <script src="script.js"></script>
-  
-</head>
-<body>
-  <h1>Module 4 Solution Starter</h1>
-  var speakWord = "Hello";
-  function speak(sam) {
-  	console.log(speakWord + "Hello" + sam);
+var message = "in global";
+console.log("global: message = " + message);
 
-  }
-(function (window) {
-	var johnGreeter = {};
-	johnGreeter.name =  "John";
-	var greeting = "Hi ";
-	johnGreeter.sayHi = function () {
-		console.log(greeting + johnGreeter.name);
-	}
-
-     window.johnGreeter = johnGreeter;
-
-}) (window);
-
-
-(function (window) {
-	var yaakovGreeter = {};
-	yaakovGreeter.name = "Yaakov";
-	var greeting = "Hello ";
-	yaakovGreeter.sayHello = function () {
-		console.log(greeting + yaakovGreeter.name);
-      }
-
-         window.yaakovGreeter = yaakovGreeter;
-
-
-})(window);
-
-
- function speak(name) {
- 	console.log(speakWord + " " + name);
- 		
- }
-
-var speakWord = "Good Bye";
-
-var byeSpeaker =  
+var a = function () {
+   var message = "inside a";
+   console.log("a: message = " + message);
+   b();
 
 
 
 
+  function b () {
+     console.log("b: message = " + message);
+}
 
 
-</body>
-</html>
+}
+
+ a();
