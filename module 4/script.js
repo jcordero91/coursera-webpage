6b34c22f2dda9f557dcd3232663da2e54d2296d4
Jasmine.Cordero <jcordero91@gmail.com>
2017-04-15 13:53:42 -0400

    MEssage

--- module 4/script.js
+++ module 4/script.js
@@ -17,3 +17,15 @@ var a = function () {
 }
 
  a();
+
+(function (window) {
+  var yaakovGreeter = {};
+  yaakovGreeter.name = "Yaakov";
+  var greeting = "Hello ";
+  yaakovGreeter.sayHello = function () {
+    console.log(greeting + yaakovGreeter.name);
+  }
+
+  window.yaakovGreeter = yaakovGreeter;
+
+})(window);
