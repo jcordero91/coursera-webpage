3c4030cec1334e0199cf127e7433acc903e00a46
Jasmine.Cordero <jcordero91@gmail.com>
2017-04-20 17:02:25 -0400

    Messge

--- module 4/script.js
+++ module 4/script.js
@@ -1,31 +1,3 @@
-var message = "in global";
-console.log("global: message = " + message);
+var "" = hi
 
-var a = function () {
-   var message = "inside a";
-   console.log("a: message = " + message);
-   b();
-
-
-
-
-  function b () {
-     console.log("b: message = " + message);
-}
-
-
-}
-
- a();
-
-(function (window) {
-  var yaakovGreeter = {};
-  yaakovGreeter.name = "Yaakov";
-  var greeting = "Hello ";
-  yaakovGreeter.sayHello = function () {
-    console.log(greeting + yaakovGreeter.name);
-  }
-
-  window.yaakovGreeter = yaakovGreeter;
-
-})(window);
+var name = "Yakkov"
